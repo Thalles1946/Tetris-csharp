d4740b08caebab3b4dfbb5c580585b936fb7698d
Thalles1946 <thallesaug2099@gmail.com>
2024-07-06 19:03:23 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Tetris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tetris!");
+
+        }
+    }
+}
